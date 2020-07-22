555eeae496265437f59852f9a2f64ff31ab7e7ee
Jonathan Freedman <jonafree@gmail.com>
2020-07-21 18:18:25 -0700

    first post :sparkling_heart:
    the `compare-lists.py` script may be used to checking various member exports against action network reports

---
+++ README.md
@@ -0,0 +1,24 @@
+# SURJ Bay Area Membership Tools
+
+These require Python3 and shoudl get wrapped in a docker container for ease of use. In the meantime, there is [virtualenv](https://virtualenv.pypa.io/en/latest/)
+
+## `compare_lists.py`
+
+This script is used to quickly compare exports of various member lists. It can compare Slack and Google Groups against the Action Network report provided. When generating the Action Network report two formats are supported. The script will attempt to compare emails however it no matching email is found it will attempt to compare against the full name, however will throw a warning with both emails in this case.
+
+* `first_name,last_name,email`
+* `first_name,last_name,email,committee`
+
+The script may be invoked in two ways.
+
+To check Google Groups membership
+
+```
+$ ./compare-lists.py audit_group action-network-report.csv google-group-members.csv
+```
+
+To check Slack membership
+
+```
+$ ./compare-lists.py audit_slack action-network-report.csv slack-members.csv
+```
---
+++ compare-lists.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+import csv
+import logging
+import os
+import fire
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+class Member(object):
+    def __init__(self, name, email, committee=None):
+        self.name = name
+        self.email = email
+        self.committee = committee
+
+    def __eq__(self, other):
+        if not isinstance(other, Member):
+            return false
+
+        if self.email.lower() == other.email.lower():
+            return True
+        else:
+            if self.name.lower() == other.name.lower():
+                LOG.warn("matched %s by name - %s / %s" % (self.name, self.email, other.email))
+                return True
+            else:
+                return False
+
+    def contains(other, others):
+        for member in others:
+            if other == member:
+                return True
+
+        return False
+
+    def __str__(self):
+        return "%s <%s>" % (self.name, self.email)
+
+    def __repr__(self):
+        return self.__str__()
+    
+class CompareLists(object):
+    def _an_reader(self, an_export):
+        if not os.path.exists(an_export):
+            raise Exception("unable to find action network export %s" % an_export)
+        an_members = []
+        with open(an_export, 'r') as an_csv:
+            for an_row in csv.reader(an_csv):
+                if an_row[0].startswith('#'):
+                    continue
+                if an_row[0] == 'first_name':
+                    continue
+                if len(an_row) == 3:
+                    [first_name, last_name, email] = an_row
+                    an_members.append(Member("%s %s" % (first_name, last_name), email))
+                elif len(an_row) == 4:
+                    [first_name, last_name, email, committee] = an_row
+                    an_members.append(Member("%s %s" % (first_name, last_name), email, committee))
+                else:
+                    raise Exception("unexpected action network report format")
+        return an_members
+
+    def audit_group(self, an_export, google_export):
+
+        if not os.path.exists(google_export):
+            raise Exception("unable to find google group membership export %s" % google_export)
+
+        group_members = []
+        with open(google_export, 'r') as g_csv:
+            for g_row in csv.reader(g_csv):
+                if g_row[0].startswith('#'):
+                    continue
+                if g_row[0].startswith('Members for group') or \
+                   g_row[0] == 'Email address':
+                    continue
+                email = g_row[0]
+                name = g_row[1]
+                group_members.append(Member(name, email))
+
+        an_members = self._an_reader(an_export)
+        LOG.info("read %s an members, %s google group members" % (len(an_members), len(group_members)))
+
+        missing_count = 0
+        for group_member in group_members:
+            if not Member.contains(group_member, an_members):
+                print("%s not found in action network" % (group_member))
+                missing_count += 1
+
+        print("%s out of %s group members are missing from action network" % (missing_count, len(group_members)))
+
+    def audit_slack(self, an_export, slack_export):
+        if not os.path.exists(slack_export):
+            raise Exception("unable to find slack export %s" % slack_export)
+
+        slack_members = []
+        with open(slack_export, 'r') as s_csv:
+            for s_row in csv.reader(s_csv):
+                if s_row[0].startswith('#'):
+                    continue
+                if s_row[0] == 'username':
+                    continue
+
+                email = s_row[1]
+                name = s_row[7]
+                slack_members.append(Member(name, email))
+
+        an_members = self._an_reader(an_export)
+        LOG.info("read %s an members, %s slack members" % (len(an_members), len(slack_members)))
+
+        missing_count = 0
+        for slack_member in slack_members:
+            if not Member.contains(slack_member, an_members):
+                print("%s not found in action network" % (slack_member))
+                missing_count += 1
+
+        print("%s out of %s slack members are missing from action network" % (missing_count, len(slack_members)))
+        
+if __name__ == '__main__':
+    fire.Fire(CompareLists)
